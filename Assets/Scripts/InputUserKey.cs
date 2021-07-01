7ffc40ac07efee5d9d7011e919ef91b82f53f909
hideru <jota.16.stigma.world@gmail.com>
2021-07-02 02:50:51 +0900

    until L33

---
+++ Assets/Scripts/InputUserKey.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputUserKey : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // To execute regularly
+        // float x = Input.GetAxis("Horizontal");
+        // float y = Input.GetAxis("Vertical");
+        // Debug.Log($"{x}, {y}");
+
+        // bool onSpace = Input.GetKey("space"); // Not Space, not Jump
+        // if (onSpace)
+        // {
+            // Debug.Log("space is pushed!");
+        // }
+    }
+}
