ab2aa8bdb6e31d34fd73eb6a30cb9d11c1eadf2c
hideru <jota.16.stigma.world@gmail.com>
2021-07-09 02:07:48 +0900

    Until L55

--- Assets/Scripts/1st-Stage/GameManagerScript1st.cs
+++ Assets/Scripts/1st-Stage/GameManagerScript1st.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManagerScript1st : MonoBehaviour
 {
     public GameObject gameOverText;
     public GameObject goalText;
+    public Text scoreText; // Not GameObject because has to change the word
+    private const int MAX_SCORE = 9999;
+    private int score = 0;
     
     // Start is called before the first frame update
     void Start()
@@ -27,5 +31,19 @@ public class GameManagerScript1st : MonoBehaviour
     public void Goal()
     {
         goalText.SetActive(true);
+    }
+
+    // public void addScore() // Not good. Obey Coding regulation that function name's first character is Capital Letter
+    public void AddScore(int value)
+    {
+        // score += 100;
+        score += value; // Change additional score depends an item the player got
+        if (score > MAX_SCORE)
+        {
+            score = MAX_SCORE;
+        }
+
+        // scoreText.text = score; // score is INT. Needs to convert to String by ToString()
+        scoreText.text = score.ToString();
     }
 }
--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class ItemManager : MonoBehaviour
 {
+    private GameManagerScript1st gameManagerScript1st;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // gameManagerScript1st = GameObject.Find("GameManagerScript1st").GetComponent<GameManagerScript1st>(); // Search at Hierarchy
+        // ^ GameObject.Find search only Object Name In HIERARCHY. Not a script name
+        gameManagerScript1st = GameObject.Find("GameManager").GetComponent<GameManagerScript1st>(); // Search at Hierarchy
     }
 
     // Update is called once per frame
@@ -18,6 +21,7 @@ public class ItemManager : MonoBehaviour
 
     public void GetItem()
     {
+        gameManagerScript1st.AddScore(100);
         Destroy(this.gameObject);
     }
 }
